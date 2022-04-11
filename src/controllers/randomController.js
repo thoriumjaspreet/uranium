6ef0239ddf2743b70e51c03defa1ffdbcac68e24
thoriumjaspreet <jaspreet98cheema@gmail.com>
2022-04-11 11:20:35 +0530

    session/node-apis

--- src/controllers/randomController.js
+++ src/controllers/randomController.js
@@ -1,9 +1,64 @@
-let addToArray= function (req, res) {
-    let x= req.body.number
-    console.log(x)
-    let arr= [2, 5, 11, 14]
-    arr.push(x)
-    res.send( {  msg: "post req 3", data: arr  } )
-}
+// let addToArray= function (req, res) {
+//     let x= req.body.number
+//     console.log(x)
+//     let arr= [2, 5, 11, 14]
+//     arr.push(x)
+//     res.send( {  msg: "post req 3", data: arr  } )
+    
+// }
 
-module.exports.addToArray= addToArray
+// let players =
+//    [
+//        {
+//            "name": "manish",
+//            "dob": "1/1/1995",
+//            "gender": "male",
+//            "city": "jalandhar",
+//            "sports": [
+//                "swimming"
+//            ]
+//        },
+//        {
+//            "name": "gopal",
+//            "dob": "1/09/1995",
+//            "gender": "male",
+//            "city": "delhi",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//        {
+//            "name": "lokesh",
+//            "dob": "1/1/1990",
+//            "gender": "male",
+//            "city": "mumbai",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//    ]
+//    router.post('/players', function (req, res) {
+ 
+//     //LOGIC WILL COME HERE
+//     res.send(  { data: players , status: true }  )
+// })
+
+
+
+
+
+
+
+
+
+// // let playerfunction=function(req,res){
+
+
+ 
+// //    res.send(  { data: players , status: true }  )
+
+// // }
+
+// // module.exports.playerfunction=playerfunction
+
+// module.exports.addToArray= addToArray
